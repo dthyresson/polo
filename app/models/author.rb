563b4a641211f9694c1530a71622152df9160159
A. David Thyresson <david@stattleship.com>
2014-06-22 19:14:17 -0400

    fix indentation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,10 @@
 class Author < ActiveRecord::Base
-	has_many :devices
+  has_many :devices
 
   validates_presence_of :name
-	validates_plausible_phone :phone_number
+  validates_plausible_phone :phone_number
 
-	phony_normalize :phone_number, :default_country_code => 'US'
+  phony_normalize :phone_number, :default_country_code => 'US'
 
   def device_id
     device = devices.first
